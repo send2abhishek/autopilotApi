1bec5803e0abe229da8c9f3e025847e45ccaf745
shailesh <shaileshshandilya15@gmail.com>
2020-06-08 22:13:55 +0530

    Add appi to change file to canonical data

--- Controllers/appController.js
+++ Controllers/appController.js
@@ -6,6 +6,7 @@ const uploadFile = async (req, res, next) => {
     const Id = new mongoose.Types.ObjectId();
     const title = req.body.title;
     const filePath = `${req.headers.host}/uploads/${req.file.originalname}`;
+    console.log("FilePath ::: ",filePath)
     const item = new itemModel({
       _id: Id,
       avatar: filePath,
@@ -32,7 +33,55 @@ const uploadFile = async (req, res, next) => {
     throw new Error(err);
   }
 };
+const xlsxFile  = require('read-excel-file/node');
+
+const convertToCanonicalData = async (req, res, next) => {
+  try {
+    console.log("chang to canonical format");
+  
+    const filePath ='C:/AAP/autopilotApi/uploads/Sample1.xlsx';
+    xlsxFile(filePath).then((rows) => {
+      console.log(rows);
+    })
+
+     
+
+    // STEP 1: Reading JSON file 
+    const users = require("./users"); 
+    
+    // Defining new user 
+    let user = { 
+      name: "New User", 
+      age: 30, 
+      language: ["PHP", "Go", "JavaScript"] 
+    }; 
+    
+    // STEP 2: Adding new data to users object 
+    users.push(user); 
+    
+    // STEP 3: Writing to a file 
+    fs.writeFile("users.json", JSON.stringify(users), err => { 
+      
+      // Checking for errors 
+      if (err) throw err; 
+    
+      console.log("Done writing"); // Success 
+    }); 
+    
+
+
+    res.status(200).json({
+      message: "File Read sucessfully",
+     
+    });
+   
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
 
 module.exports = {
   uploadFile,
+  convertToCanonicalData,
 };
